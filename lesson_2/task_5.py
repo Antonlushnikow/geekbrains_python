7890e90606c90db107eb885018b421847813394e
antonlushnikow <antonlushnikow@gmail.com>
2020-10-29 23:46:22 +0500

    deleted all the files

--- lesson_2/task_5.py
+++
@@ -1,9 +0,0 @@
-my_list = [7, 5, 3, 3, 2]
-new_el = int(input('Введите новый элемент: '))
-# new_el = 1
-index = 0
-for i in range(len(my_list)):
-    if new_el <= my_list[i]:
-        index = i + 1
-my_list.insert(index, new_el)
-print(my_list)
